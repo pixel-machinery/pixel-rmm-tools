e280612217724836f5c6571f548da8c15ee412a8
Vlad Grenkov <vlad@pixelmachinery.com>
2022-03-08 16:02:37 -0500

    added macos version logic

--- Mac/reboot.sh
+++ Mac/reboot.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env zsh
+
+
 
 # IBM Notifier binary paths
 NA_PATH="/Applications/Pixel Notifier.app/Contents/MacOS/Pixel Notifier"
@@ -6,29 +8,74 @@ NA_PATH="/Applications/Pixel Notifier.app/Contents/MacOS/Pixel Notifier"
 # Variables for the popup notification for ease of customization
 
 # This check whether the plist counter exists, creates with zero count if not. 
-POPUP_COUNTER_CMD="defaults read com.pixelmachinery.notifier popup_count"
-if ($POPUP_COUNTER_CMD) > /dev/null 2>&1; then
-    POPUP_COUNTER=$($POPUP_COUNTER_CMD)
-    echo "Popup counter plist found with value ${POPUP_COUNTER}"
-else
-    echo "Popup counter plist not found, creating with zero count..."
-    defaults write com.pixelmachinery.notifier popup_count 0
-    POPUP_COUNTER=$($POPUP_COUNTER_CMD)
-fi
-echo "Current count is: ${POPUP_COUNTER}"
-COUNTER_LIMIT=2
-POSTPONES_LEFT=$((COUNTER_LIMIT-POPUP_COUNTER))
-WINDOWTYPE="popup"
-BAR_TITLE="Pixel Machinery Notification"
-TITLE="Reboot required"
-TIMEOUT="" # leave empty for no notification time
-BUTTON_1="Update & Restart Now"
-BUTTON_2="Postpone one day (${POSTPONES_LEFT} left)"
-SUBTITLE="Your Mac needs to be restarted to apply important updates. Please save your work and restart at your earliest convenience. 
+# $(/usr/libexec/PlistBuddy -c 'print ":name"' 
+# defaults read com.pixelmachinery.notifier popup_count
+# POPUP_COUNTER_CMD="bash -c `defaults read com.pixelmachinery.notifier popup_count`"
+# echo "$($POPUP_COUNTER_CMD)"
+ACTUAL=$(sw_vers -productVersion)
+LATEST_MONTEREY="12.2.1"
+LATEST_BIGSUR="11.6.4"
+LATEST_CATALINA="10.15.7"
 
-Note that the update process may take up to an hour, please make sure your laptop is plugged in to power."
+LATEST_MONTEREY_BUILD="21D62"
+LATEST_BIGSUR_BUILD="20G417"
+LATEST_CATALINA_BUILD="19H1715"
 
 ### FUNCTIONS ###
+
+upgrade_check() {
+
+    
+
+    PATH="/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin"
+
+            # this will check to make sure `sw_vers` exists
+            # if it does not, this is probably not macOS
+    if ((! $+commands[sw_vers] ))
+    then
+            echo "$NAME: 'sw_vers' is required but not found in $PATH" >>/dev/stderr
+            exit 2
+    fi
+
+            ## First we get the value for this Mac and save it to `$ACTUAL`
+    
+
+            ## load 'is-at-least' so we can use it
+    autoload is-at-least
+
+            ## "Is the version of macOS that we are using _at least_ 10.16?"
+    if [[ "$ACTUAL" == 10.15.* ]]; then
+        # echo "macOS Catalina - $ACTUAL"
+        if is-at-least "$LATEST_CATALINA" "$ACTUAL"; then
+            # echo "We're on latest version - $LATEST_CATALINA"
+            echo "0"
+        else
+            # echo "Not on latest, should upgrade to - $LATEST_CATALINA"
+            echo $LATEST_CATALINA_BUILD
+        fi
+    elif [[ "$ACTUAL" == 11.* ]]; then
+        # echo "macOS Big Sur - $ACTUAL"
+        if is-at-least "$LATEST_BIGSUR" "$ACTUAL"; then
+            # echo "We're on latest version - $LATEST_BIGSUR"
+            echo "0"
+        else
+            # echo "Not on latest, should upgrade to - $LATEST_BIGSUR"
+            echo $LATEST_BIGSUR_BUILD
+        fi
+    elif [[ "$ACTUAL" == 12.* ]]; then
+        # echo "macOS Monterey - $ACTUAL"
+        if is-at-least "$LATEST_MONTEREY" "$ACTUAL"; then
+            # echo "We're on latest version - $LATEST_MONTEREY"
+            echo "0"
+        else
+            # echo "Not on latest, should upgrade to - $LATEST_MONTEREY"
+            echo $LATEST_MONTEREY_BUILD
+        fi
+    else
+        # echo "(Mac) OS X something -- probably pre-catalina"
+        echo "1"
+    fi
+}
 
 prompt_user() {
     # This will call the IBM Notifier Agent
@@ -42,10 +89,10 @@ prompt_user() {
         -subtitle "${SUBTITLE}" \
         -timeout "${TIMEOUT}" \
         -main_button_label "${BUTTON_1}" \
-        "${sec_button[@]}" \
+        ${sec_button[@]} \
         -always_on_top \
         -accessory_view_type timer \
-        -accessory_view_payload "Time left until restart: %@" -timeout 500
+        -accessory_view_payload "Time left until restart: %@" -timeout 5000
         )
 
     echo "$?"
@@ -53,22 +100,49 @@ prompt_user() {
 
 ### END FUNCTIONS ###
 
-# Example 1 button prompt
+# Check if update is needed...
+UPGRADE_COMMAND=$(upgrade_check)
+echo "$UPGRADE_COMMAND"
 
-# Example 2 button prompt
-NEW_COUNTER=$((POPUP_COUNTER+1))
+if [ ! "$UPGRADE_COMMAND" = "0" ]; then
+    echo "Running upgrade logic, upgrading to: $UPGRADE_COMMAND"
+    if [ $(defaults read com.pixelmachinery.notifier popup_count) ]; then
+        POPUP_COUNTER=$(defaults read com.pixelmachinery.notifier popup_count)
+        echo "Popup counter plist found with value ${POPUP_COUNTER}"
+    else
+        echo "Popup counter plist not found, creating with zero count..."
+        defaults write com.pixelmachinery.notifier popup_count 0
+        POPUP_COUNTER=$(defaults read com.pixelmachinery.notifier popup_count)
+    fi
+    echo "Current count is: ${POPUP_COUNTER}"
 
-RESPONSE=$(prompt_user)
-echo "$RESPONSE"
-if [ $RESPONSE -eq "0" ]; then
-    echo "Reboot button pressed"
-    echo "Resetting counter to 0"
-    defaults write com.pixelmachinery.notifier popup_count 0
-elif [ $RESPONSE -eq "2" ]; then
-    echo "Postpone button pressed."
-    defaults write com.pixelmachinery.notifier popup_count $NEW_COUNTER
-else
-    echo "Time ran out, forcing reboot."
-    echo "Resetting counter to 0"
-    defaults write com.pixelmachinery.notifier popup_count 0
+    NEW_COUNTER=$((POPUP_COUNTER+1))
+
+
+    COUNTER_LIMIT=2
+    POSTPONES_LEFT=$((COUNTER_LIMIT-POPUP_COUNTER))
+    WINDOWTYPE="popup"
+    BAR_TITLE="Pixel Machinery Notification"
+    TITLE="Reboot required"
+    TIMEOUT="" # leave empty for no notification time
+    BUTTON_1="Update & Restart Now"
+    BUTTON_2="Postpone one day (${POSTPONES_LEFT} left)"
+    SUBTITLE="Your Mac needs to be restarted to apply important updates (from ${ACTUAL} to build ${UPGRADE_COMMAND}). Please save your work and restart at your earliest convenience.
+
+    Note that the update process may take up to an hour, please make sure your laptop is plugged in to power."
+
+    RESPONSE=$(prompt_user)
+    echo "$RESPONSE"
+    if [ $RESPONSE -eq "0" ]; then
+        echo "Reboot button pressed"
+        echo "Resetting counter to 0"
+        defaults write com.pixelmachinery.notifier popup_count 0
+    elif [ $RESPONSE -eq "2" ]; then
+        echo "Postpone button pressed."
+        defaults write com.pixelmachinery.notifier popup_count $NEW_COUNTER
+    else
+        echo "Time ran out, forcing reboot."
+        echo "Resetting counter to 0"
+        defaults write com.pixelmachinery.notifier popup_count 0
+    fi
 fi
